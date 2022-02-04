e1544334aba06bda3d0ffe9610da744c1c33e467
Matteo <gnappo.1@live.it>
2022-02-03 16:39:22 -0800

    commit tree 401086bfb6114b2bf1d36e004f34cfa127d85d80

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,21 @@
+require "pry"
 def starts_with_a_vowel?(word)
-
+ /^[aeiou]/i.match?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.scan /\bun\w+ing\b/ # try to get to work with ^$
 end
 
 def words_five_letters_long(text)
-
+    text.scan /\b\w{5}\b/
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    /^[A-Z].+[\.!?]$/.match? text # try to get to work with \b
 end
 
 def valid_phone_number?(phone)
-
+    binding.pry
+    /(\d.*){10}|(\(\d{3}\)((\d{3}-\d{4})|\d{7}))/.match phone
 end
